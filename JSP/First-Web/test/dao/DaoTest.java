914c4194ff721321a0d01730d24b3b4b99c25f07
parksangmyeong1 <psm950324@naver.com>
2021-07-30 17:41:37 +0900

    DeptdaoTestCase

--- JSP/First-Web/test/dao/DaoTest.java
+++ JSP/First-Web/test/dao/DaoTest.java
@@ -50,29 +50,23 @@ public class DaoTest {
 	public void testGetDeptList() {
 		// 리스트 반환된건지 확인
 		assertNotNull("리스트 값이 있는지 확인", dao.getDeptList(conn));
-		// 리스트 출력
-		List<Dept> list = dao.getDeptList(conn);
-		for(int i=0;i<list.size();i++) {
-			System.out.println(list.get(i));
-		}
+		
 	}
 	
 	@Test
-	public void testInsertDao() {
+	public void testInsertDept() {
 		// 반환되는 resultCnt가 1이면 테스트 성공
 		assertSame("dept가 입력되지 않은 경우", 1, dao.insertDept(conn, new Dept(50, "개발", "제주도")));
 	}
 	
 	@Test
-	public void testUpdateDao() {
-		// 해당 deptno에 리스트가 반환되는지 확인
-		// assertNotNull("dept가 반환되지 않은 경우", dao.selectByDeptno(conn, 50));
+	public void testUpdateDept() {
 		// 수정한 내용이 업데이트 되면 1
 		assertSame("dept가 수정되지 않은 경우", 1, dao.updateDept(conn, new Dept(50, "개발팀", "서울")));
 	}
 	
 	@Test
-	public void testDeleteDao() {
+	public void testDeleteDept() {
 		// 삭제가 되면 1 
 		assertSame("dept가 삭제되지 않은 경우", 1, dao.deleteDept(conn, 50));
 	}
