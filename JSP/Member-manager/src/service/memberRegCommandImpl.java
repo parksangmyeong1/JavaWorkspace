09b026479bb1980d63aa91d8ac72305d4ed992cc
parksangmyeong1 <psm950324@naver.com>
2021-07-31 00:14:26 +0900

    mvc

--- JSP/Member-manager/src/service/memberRegCommandImpl.java
+++ JSP/Member-manager/src/service/memberRegCommandImpl.java
@@ -7,6 +7,8 @@ import java.sql.SQLException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.fileupload.FileUploadException;
+
 import dao.MemberDao;
 import util.ConnectionProvider;
 import util.JdbcUtil;
@@ -16,29 +18,12 @@ public class memberRegCommandImpl implements Command {
 	@Override
 	public String getPage(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		
-		// dept_list.jsp -> 요청을 받고 처리 -> 결과 데이터를 객체속성에 저장 -> view 지정
-
-		// 1. 드라이버 로드
-		
-		// 2. DB 연결
-		Connection conn = null;
-		MemberDao dao = MemberDao.getInstance();
-		
 		try {
-			// jdbcUrl 
-			conn = ConnectionProvider.getConnection();
-			
-			// 6. 결과 데이터 request 의 속성에 저장 -> 데이터 공유(전달)
-			request.setAttribute("result", dao.selectList(conn));
-			
-		} catch (SQLException e){
+			request.setAttribute("resultReg", MemberRegService.getInstance().regMember(request));
+		} catch (FileUploadException e) {
 			e.printStackTrace();
-		} catch (Exception e){
-			e.printStackTrace();
-		} finally{
-			JdbcUtil.close(conn);
 		}
-		// <jsp:forward page="list_view.jsp" />
-		return "/WEB-INF/views/list_view.jsp";
+		
+		return "/WEB-INF/views/reg_view.jsp";
 	}
 }
