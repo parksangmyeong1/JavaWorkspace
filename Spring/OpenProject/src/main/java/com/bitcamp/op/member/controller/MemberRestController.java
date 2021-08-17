aa8b706bf0ec10946537e61cc2dd03569434ffbf
parksangmyeong1 <psm950324@naver.com>
2021-08-17 20:26:16 +0900

    spring Rest API

---
+++ Spring/OpenProject/src/main/java/com/bitcamp/op/member/controller/MemberRestController.java
@@ -0,0 +1,52 @@
+package com.bitcamp.op.member.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bitcamp.op.member.domain.Member;
+import com.bitcamp.op.member.service.MemberRestService;
+
+// @Controller
+@RestController
+public class MemberRestController {
+
+	@Autowired
+	private MemberRestService service;
+	
+	@RequestMapping("/members/{id}")
+	// @ResponseBody
+	public Member getMember(
+			@PathVariable("id") int idx) {
+		
+		Member member = service.getMember(idx);
+		System.out.println(member);
+		
+		return member;
+	}
+	
+	/*
+	 * @DeleteMapping
+	 * 
+	 * @PutMapping
+	 * 
+	 * @PostMapping
+	 */
+	@GetMapping("/members")
+	public List<Member> getMembers(){
+		return service.getMembers();
+	}
+	
+	@GetMapping("/members1")
+	public Map<Integer, Member> getMembers1(){
+		
+		return service.getMembers1();
+	}
+	
+}
