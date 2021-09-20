2a6ce18dd730190f7477ebd1e284db4fc5476ad0
parksangmyeong1 <psm950324@naver.com>
2021-09-20 23:22:48 +0900

    210920 COB

--- SQL/mysql/210917cob.sql
+++ SQL/mysql/210917cob.sql
@@ -7,4 +7,5 @@ select count(*) from post where postWriter like '%n%';
 select * from post where postWriter like '%n%';
 select count(*) from post where postTitle like '%테스트%' or postContent like '%테스트%';
 select * from post order by postIdx desc limit 0,10;
-select count(*) from post where postSort = '잡담';
+select count(*) from post where postSort = '잡담' and postWriter like '%테스트%';
+
